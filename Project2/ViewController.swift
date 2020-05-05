437664629b924f7a8b37c9fb4b27d06ff02b7066
Janusz <janklyz@gmail.com>
2020-05-05 11:20:05 +0200

    Day 20, shuffle cards and IBAction, Alert

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -16,12 +16,15 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
    countries += ["estonia", "france", "germany", "ireland", "italy", "monaco",
                  "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
@@ -30,14 +33,36 @@ class ViewController: UIViewController {
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
 
-
-        askQuesyion()
+        askQuestion()
+        
     }
-    func askQuesyion(){
+
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
 
+        title = countries[correctAnswer].uppercased()
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+      var title: String
+
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        print(score)
+
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 }
 
